505941335f672549c31bf3fc754e894cf84bcc69
grupo001efg <163312970+grupo001efg@users.noreply.github.com>
2024-09-11 10:24:32 -0300

    versao

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function validateForm(){
      
-    var email= document.getElementByld("email").value;
-    var password= document.getElementByld("password").value;
+    var email = document.getElementByld("email").value;
+    var password = document.getElementByld("password").value;
 
 
-if(email ==="" || password ==="") {
-    alert("Por favor, preencha todos os campos.");
-    return false;
-}
+    if(email === "" || password === "") {
+        alert("Por favor, preencha todos os campos.");
+        return false;
+    }
     return true;
- }
+}
 
