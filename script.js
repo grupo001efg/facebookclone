b43f58c2e9b2f55c6f00ebf1726e548e63a5ba07
grupo001efg <163312970+grupo001efg@users.noreply.github.com>
2024-09-11 10:14:22 -0300

    facebook.003

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function validateForm(){
+     
+    var email= document.getElementByld("email").value;
+    var password= document.getElementByld("password").value;
+
+
+if(email ==="" || password ==="") {
+    alert("Por favor, preencha todos os campos.");
+    return false;
+}
+    return true;
+ }
 
